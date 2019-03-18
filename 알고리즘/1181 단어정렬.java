57e8b146a9443584f571fdd695ed82466bb72515
Sa Eun Soo <42709747+Rory0304@users.noreply.github.com>
2019-03-19 00:33:13 +0900

    Update 1181 단어정렬.java
    Arrays.sort(arrays,i,j) 사용

--- 알고리즘/1181 단어정렬.java
+++ 알고리즘/1181 단어정렬.java
@@ -15,9 +15,9 @@ public class Main {
 		
 		//{but,or,and,no,na,no};
 		//사전 기준으로 배열을 한다. -> {and,but,na,no,no,or}
-		Arrays.sort(words);
+		// Arrays.sort(words);
 		
-		//문자 길이 순으로 우선 배열한다.
+		//문자 길이 순으로 우선 배열함, 길이가 같으면 그 범위 내에서 사전순으로 배열
 		for(int i=n-1;i>0;i--) {
 			for(int j=0;j<i;j++) {
 				if(words[j].length() > words[j+1].length()) {
@@ -25,18 +25,23 @@ public class Main {
 					words[j+1] = words[j];
 					words[j] = tmp;
 				}
+				if(words[j].length() == words[j+1].length()){
+					Arrays.sort(words,j,j+2);
+				}
 			}
 		}
 		
 		//같은 문자일 경우 제거를 한다.
-		for(int i=0;i<n-1;i++) {
-			if(words[i].equals(words[i+1])) {
-				words[i] = null;
-			}
-		}
+// 		for(int i=0;i<n-1;i++) {
+// 			if(words[i].equals(words[i+1])) {
+// 				words[i] = null;
+// 			}
+// 		}
 		
-		for(int i=0;i<n;i++) {
-			if(words[i] != null) {
+	//같은 문자일 경우 넘어간다. 결과 출력
+		for(int i=0;i<n-1;i++) {
+			if(words[i].equals(words[i+1])) continue;
+			else {
 				System.out.println(words[i]);
 			}
 		}
